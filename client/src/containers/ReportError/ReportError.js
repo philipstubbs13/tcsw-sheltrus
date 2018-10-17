94dcfbf2e897e58d2f3998cf815e04b72b0bea0a
philipstubbs13 <philipstubbs13@gmail.com>
2018-10-17 06:06:00 -0500

    added snackbar component

--- client/src/containers/ReportError/ReportError.js
+++ client/src/containers/ReportError/ReportError.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 // import prop types
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 // Import Material UI components and styling
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 // import database
 import { database } from '../../firebase-config';
 // import css
@@ -43,12 +50,93 @@ const styles = {
   },
 };
 
+const variantIcon = {
+  success: CheckCircleIcon,
+};
+
+const styles1 = theme => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+function MySnackbarContent(props) {
+  const {
+    classes,
+    className,
+    message,
+    onClose,
+    variant,
+    ...other
+  } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={classNames(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={(
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={classNames(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+)}
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={onClose}
+        >
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  );
+}
+
+MySnackbarContent.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  message: PropTypes.node,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
+};
+
+const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
+
+const styles2 = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+});
+
 class ReportError extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      errorlocation: '',
+      errorLocation: '',
       errorDescription: '',
+      open: false,
     };
 
     this.reportErrorsRef = database.ref('/reporterrors');
@@ -74,6 +162,20 @@ class ReportError extends Component {
       email,
       name,
     });
+
+    this.setState({ open: true });
+  };
+
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
   };
 
   render() {
@@ -165,6 +267,21 @@ class ReportError extends Component {
               >
                 Report
               </Button>
+              <Snackbar
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                open={this.state.open}
+                autoHideDuration={6000}
+                onClose={this.handleClose}
+              >
+                <MySnackbarContentWrapper
+                  onClose={this.handleClose}
+                  variant="success"
+                  message="This is a success message!"
+                />
+              </Snackbar>
             </form>
           </Grid>
         </Grid>
